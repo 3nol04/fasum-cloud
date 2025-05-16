c528c210eca23976e63dce515eb8b7976f77a9e8
TriSetianto123 <156288410+3nol04@users.noreply.github.com>
2025-05-16 09:23:24 +0700

    update

--- index.js
+++ index.js
@@ -1,66 +1,71 @@
-const express = require("express");
-const admin = require("firebase-admin");
-const serviceAccountKey = require("./servicesAccounKey.json");
-const cors = require = require
-require("dotenv/config");
+const express = require('express');
+const admin = require('firebase-admin');
+const bodyParser = require('body-parser');
+const serviceAccount = require('./serviceAccountKey.json');
+const cors = require('cors');
+require('dotenv').config();
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountKey),
+    credential: admin.credential.cert(serviceAccount),
 });
 
-const app = express(
-    app.use(bodyParse.json()));
+const app = express();
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/' , (req, res) => {
-    res.send("Hai. ini adalah res api dari fasum")
-})
+app.get('/', (req, res) => {
+    res.send('Hai, ini adalah REST API untuk Fasum!');
+});
 
+app.post('/send-to-topic', async (req, res) => {
+    const { topic, title, body, senderName, senderPhotoUrl } = req.body;
 
-        app.post("/sent-to-topic", async (req, res) => {
-        const {topic, title,body, senderName,senderPhotoUrl} = req.body;
-        if(!topic || !title || !body){
-            return res.status(400).json({message:"token,  title dan body harus diisi"})
-        }
+    if (!topic || !title || !body ) {
+        return res.status(400).send({error: 'token, title, dan body wajib diisi.'});
+    }
 
-        const massage = {
-            notification: {
-                title : title ,
-                body : body,
-            },
-            data: {
-                title : title || "Notifikasi Baru", 
-                body : body || "Anda memiliki notifikasi baru",
-                senderName : senderName || "Admin",
-                senderPhotoUrl : senderPhotoUrl || "",
-                sandAt : Date.now(),
-                massageType : "notification"
-            },
-            android :{
-                priority : "high",
-            },apns: {
-                headers:{
-                    "apns-priority": "10",
-                }
+    const message = {
+        notification: {
+            title: title,
+            body: body,
+        },
+        data:{
+            title: title || 'Notifikasi Baru',
+            body: body || 'Anda memiliki notifikasi baru',
+            senderName: senderName || 'Admin',
+            senderPhotoUrl: senderPhotoUrl || '',
+            sendAt: new Date().toISOString(),
+            messageType: 'topic-notification',
+        },
+        android: {
+            priority: 'high',
+        },
+        apns: {
+            headers: {
+                'apns_priority' : '10'
             }
         }
+    }
 
-        try{
-            const response = await admin.messaging().send(massage);
-            res.status(200).json({
-                message:"berhasil mengirim notifikasi",
-                success: true,
-                response
-            })
-        }catch(err){
-            res.status(500).json({
-                message:"gagal mengirim notifikasi",
-                success: false,
-                err
-            })
-        }
-    });
+    try {
+        const response = await admin.messaging().send(message);
+        res.status(200).json({
+            success: true,
+            message: `Notifikasi berhasil dikirim ke topic ${topic}`,
+            response
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: error.message
+        });
+    }
+});
 
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
